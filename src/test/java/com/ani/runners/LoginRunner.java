73def46cf7cc40859a1d0a724daf35abd05a15a9
Neha Kulkarni <nehakulkarni@Nehas-MacBook-Pro.local>
2021-03-10 17:34:16 -0500

    refactoring code

--- src/test/java/com/ani/runners/LoginRunner.java
+++ src/test/java/com/ani/runners/LoginRunner.java
@@ -1,9 +1,9 @@
-package com.ani.login;
+package com.ani.runners;
 
 import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-public class MyRunner {
+public class LoginRunner {
 	
 }
