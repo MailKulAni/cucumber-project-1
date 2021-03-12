881829dfb4ba5d5ea3a1b8c4acdab095b0fecd80
Neha Kulkarni <nehakulkarni@Nehas-MacBook-Pro.local>
2021-03-12 15:26:36 -0500

    added dryRun as CucumberOptions

--- src/test/java/com/ani/login/LoginRunner.java
+++ src/test/java/com/ani/login/LoginRunner.java
@@ -2,8 +2,12 @@ package com.ani.login;
 
 import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(
+		dryRun=true
+)
 public class LoginRunner {
 	
 }
